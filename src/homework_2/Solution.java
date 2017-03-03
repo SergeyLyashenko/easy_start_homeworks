4f8b78da30bf3d624e396bf0290049ce7b60716f
SergeyLyashenko <inlyashenko@gmail.com>
2017-03-03 22:25:19 +0200

    //

--- src/homework_2/Solution.java
+++ src/homework_2/Solution.java
@@ -3,18 +3,19 @@ package homework_2;
 
 public class Solution {
     public static void main(String[] args) {
-        ListNode list1 = new ListNode(2);
-        list1.next = new ListNode(4);
-        list1.next.next = new ListNode(3);
+        ListNode list1 = new ListNode(5);
+        list1.next = new ListNode(9);
+        list1.next.next = new ListNode(9);
+        list1.next.next.next = new ListNode(9);
         ListNode list2 = new ListNode(5);
-        list2.next = new ListNode(6);
-        list2.next.next = new ListNode(4);
 
         ListNode list3 = addTwoNumbers(list1, list2);
         System.out.println(list3.val);
         System.out.println(list3.next.val);
         System.out.println(list3.next.next.val);
-        //System.out.println(list3.next.next.next.val);
+        System.out.println(list3.next.next.next.val);
+        System.out.println(list3.next.next.next.next.val);
+
 
     }
 
@@ -24,11 +25,15 @@ public class Solution {
         ListNode tempNode = new ListNode(temp);
         if (temp > 0 || l1.next != null || l2.next != null) {
             result.next = tempNode;
+        } else {
+            return result;
         }
 
+        ListNode emptyNode = new ListNode(0);
+
         while (l1.next != null || l2.next != null) {
-            l1 = l1.next != null ? l1.next : new ListNode(0);
-            l2 = l2.next != null ? l2.next : new ListNode(0);
+            l1 = l1.next != null ? l1.next : emptyNode;
+            l2 = l2.next != null ? l2.next : emptyNode;
             tempNode.val = (temp + l1.val + l2.val) % 10;
             temp = (temp + l1.val + l2.val) / 10;
             if (temp > 0 || l1.next != null || l2.next != null) {
