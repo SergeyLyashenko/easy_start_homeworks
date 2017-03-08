d6a9238e22d5beecf762585163787de104b69543
SergeyLyashenko <inlyashenko@gmail.com>
2017-03-08 22:47:48 +0200

    //

--- src/homework_3/ParseHtml.java
+++ src/homework_3/ParseHtml.java
@@ -6,8 +6,8 @@ import java.util.Stack;
 public class ParseHtml {
     public String parseHtmlDoc(StringBuilder htmlDoc) {
         StringBuilder builder = new StringBuilder();
-        Stack<WhereAmI> stack = new Stack<>();
-        stack.push(WhereAmI.BETWEEN_TAGS);
+        Stack<Position> stack = new Stack<>();
+        stack.push(Position.BETWEEN_TAGS);
         char temp;
 
         for (int i = 0; i < htmlDoc.length(); i++) {
@@ -15,46 +15,71 @@ public class ParseHtml {
             temp = htmlDoc.charAt(i);
 
             if (temp == '<' && htmlDoc.charAt(i + 1) == 's' && htmlDoc.charAt(i + 2) == 'c'
-                && (stack.peek() != WhereAmI.INSIDE_COMMENT_WITH_SINGLE_QUOTES
-                    || stack.peek() != WhereAmI.INSIDE_COMMENT_WITH_DOUBLE_QUOTES)) {
-                stack.push(WhereAmI.INSIDE_SCRIPT_TAG);
+                    && (stack.peek() != Position.INSIDE_COMMENT_WITH_SINGLE_QUOTES
+                    || stack.peek() != Position.INSIDE_COMMENT_WITH_DOUBLE_QUOTES)) {
+                stack.push(Position.INSIDE_SCRIPT_TAG);
                 continue;
             }
             if (temp == '<' && htmlDoc.charAt(i + 1) == '/' && htmlDoc.charAt(i + 2) == 's' && htmlDoc.charAt(i + 3) == 'c'
-                    && stack.peek() == WhereAmI.INSIDE_SCRIPT_TAG) {
+                    && stack.peek() == Position.INSIDE_SCRIPT_TAG) {
                 stack.pop();
                 i += 8;
                 continue;
             }
-            if (stack.peek() == WhereAmI.INSIDE_SCRIPT_TAG) {
+            if (stack.peek() == Position.INSIDE_SCRIPT_TAG) {
                 continue;
             }
-            if (temp == '<' && (stack.peek() != WhereAmI.INSIDE_COMMENT_WITH_SINGLE_QUOTES
-                    || stack.peek() != WhereAmI.INSIDE_COMMENT_WITH_DOUBLE_QUOTES)) {
-                stack.push(WhereAmI.INSIDE_TAG);
+            if (temp == '<' && htmlDoc.charAt(i + 1) == 's' && htmlDoc.charAt(i + 2) == 't'
+                    && (stack.peek() != Position.INSIDE_COMMENT_WITH_SINGLE_QUOTES
+                    || stack.peek() != Position.INSIDE_COMMENT_WITH_DOUBLE_QUOTES)) {
+                stack.push(Position.INSIDE_STYLE_TAG);
                 continue;
             }
-            if (temp == '>' && stack.peek() == WhereAmI.INSIDE_TAG) {
+            if (temp == '<' && htmlDoc.charAt(i + 1) == '/' && htmlDoc.charAt(i + 2) == 's' && htmlDoc.charAt(i + 3) == 't'
+                    && stack.peek() == Position.INSIDE_STYLE_TAG) {
+                stack.pop();
+                i += 7;
+                continue;
+            }
+            if (stack.peek() == Position.INSIDE_STYLE_TAG) {
+                continue;
+            }
+            if (temp == '<' && (stack.peek() != Position.INSIDE_COMMENT_WITH_SINGLE_QUOTES
+                    || stack.peek() != Position.INSIDE_COMMENT_WITH_DOUBLE_QUOTES)) {
+                stack.push(Position.INSIDE_TAG);
+                continue;
+            }
+            if (temp == '>' && stack.peek() == Position.INSIDE_TAG) {
                 stack.pop();
                 continue;
             }
-            if (temp == '\'' && stack.peek() != WhereAmI.INSIDE_COMMENT_WITH_SINGLE_QUOTES) {
-                stack.push(WhereAmI.INSIDE_COMMENT_WITH_SINGLE_QUOTES);
+            if (temp == '\'' && stack.peek() != Position.INSIDE_COMMENT_WITH_SINGLE_QUOTES) {
+                stack.push(Position.INSIDE_COMMENT_WITH_SINGLE_QUOTES);
                 continue;
             }
-            if (temp == '\'' && stack.peek() == WhereAmI.INSIDE_COMMENT_WITH_SINGLE_QUOTES) {
+            if (temp == '\'' && stack.peek() == Position.INSIDE_COMMENT_WITH_SINGLE_QUOTES) {
                 stack.pop();
                 continue;
             }
-            if (temp == '\"' && stack.peek() != WhereAmI.INSIDE_COMMENT_WITH_DOUBLE_QUOTES) {
-                stack.push(WhereAmI.INSIDE_COMMENT_WITH_DOUBLE_QUOTES);
+            if (temp == '\"' && stack.peek() != Position.INSIDE_COMMENT_WITH_DOUBLE_QUOTES) {
+                stack.push(Position.INSIDE_COMMENT_WITH_DOUBLE_QUOTES);
                 continue;
             }
-            if (temp == '\"' && stack.peek() == WhereAmI.INSIDE_COMMENT_WITH_DOUBLE_QUOTES) {
+            if (temp == '\"' && stack.peek() == Position.INSIDE_COMMENT_WITH_DOUBLE_QUOTES) {
                 stack.pop();
                 continue;
             }
-            if (stack.peek()==WhereAmI.BETWEEN_TAGS) {
+            if (temp == '&' && stack.peek() == Position.BETWEEN_TAGS) {
+                stack.push(Position.ENTITY_CODE);
+                continue;
+            }
+            if (temp == ';' && stack.peek() == Position.ENTITY_CODE) {
+                stack.pop();
+                continue;
+            }
+            if (stack.peek() == Position.BETWEEN_TAGS) {
+                if (temp == ' ' && (builder.length() == 0 || builder.charAt(builder.length() - 1) == ' '))
+                    continue;
                 builder.append(temp);
             }
         }
--- src/homework_3/Position.java
+++ src/homework_3/Position.java
@@ -1,9 +1,12 @@
 package homework_3;
 
-public enum WhereAmI {
+public enum Position {
     INSIDE_TAG,
     INSIDE_COMMENT_WITH_SINGLE_QUOTES,
     INSIDE_COMMENT_WITH_DOUBLE_QUOTES,
     BETWEEN_TAGS,
-    INSIDE_SCRIPT_TAG
+    INSIDE_SCRIPT_TAG,
+    INSIDE_STYLE_TAG,
+    ENTITY_CODE
+
 }
